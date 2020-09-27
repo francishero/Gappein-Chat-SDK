e397b54825dfae34dcfc4bbe1a1a44c5a428db62
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-27 15:22:51 +0530

    Added Preview for Compose

--- gappein-compose/src/main/java/com/himanshucodes/gappein/compose/ChannelList.kt
+++ gappein-compose/src/main/java/com/himanshucodes/gappein/compose/ChannelList.kt
@@ -1,15 +1,37 @@
 package com.himanshucodes.gappein.compose
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 import com.gappein.sdk.client.ChatClient
+import com.gappein.sdk.model.Channel
+import com.gappein.sdk.util.toChannelList
 
 @Composable
 fun ChannelList() {
     ChatClient.getInstance().getAllChannels { channel ->
-        LazyColumnFor(channel, modifier = Modifier) {
+        LazyColumnFor(channel, modifier = Modifier, itemContent = {
+            channelList(it)
+        })
+    }
+}
 
-        }
+@Composable
+private fun channelList(channel: Channel) {
+    Row(Modifier.padding(16.dp)) {
+
+    }
+}
+
+@Preview
+@Composable
+fun check() {
+    val channel = Channel()
+    channel.toChannelList {
+
     }
 }
